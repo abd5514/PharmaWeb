1f9eb770d1b57d98ed59626f56fb779997a231c2
abd5514 <abd147@outlook.com>
2025-08-20 14:33:09 +0300

    add sharedmethod class

--- src/test/java/org/tab/tests/LoginPageTest.java
+++ src/test/java/org/tab/tests/LoginPageTest.java
@@ -3,10 +3,12 @@ package org.tab.tests;
 import org.tab.base.Base;
 import org.tab.utils.ExtentReport.ExtentTestListener;
 import org.tab.web_pages.LoginPage;
+import org.testng.Assert;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 import static org.tab.data.TestDataReader.getXMLData;
+import static org.tab.utils.common.SharedMethods.waitUntilElementClickable;
 
 @Listeners(ExtentTestListener.class)
 public class LoginPageTest extends Base {
@@ -17,6 +19,6 @@ public class LoginPageTest extends Base {
         loginPage.emailInput.sendKeys(getXMLData("username"));
         loginPage.passwordInput.sendKeys(getXMLData("password"));
         loginPage.loginBtn.click();
-        loginPage.keepLoginCheckbox.click();
+        Assert.assertEquals(driver.getCurrentUrl(), getXMLData("url"));
     }
 }
